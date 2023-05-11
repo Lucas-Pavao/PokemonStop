fe838216a5e0fad82606d4ae22254a4d1b5e8f75
Irwing Joshua <joshuairwing@gmail.com>
2023-05-11 00:56:54 -0300

    Adição da tela de detalhes da captura

--- app/src/main/java/pdm/pokemonStop/DetailsActivity.java
+++ app/src/main/java/pdm/pokemonStop/DetailsActivity.java
@@ -3,6 +3,8 @@ package pdm.pokemonStop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -10,7 +12,36 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        
+
+        //Botão de voltar
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Inicializando elementos do layout
+
+        //Imagem
+        ImageView imageView = findViewById(R.id.pokemon_image);
+        imageView.setImageResource(R.drawable.pokemon_image);
+
+        //Nome
+        TextView nomeTextView = findViewById(R.id.pokemon_name);
+        nomeTextView.setText("Nome do Pokémon");
+
+        //Número
+        TextView numeroTextView = findViewById(R.id.pokemon_number);
+        numeroTextView.setText("Número do Pokémon");
+
+        //Tipo
+        TextView tipoTextView = findViewById(R.id.pokemon_type);
+        tipoTextView.setText("Tipo do Pokémon");
+
+    }
+
+    //Método do botão de voltar
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 
 }
